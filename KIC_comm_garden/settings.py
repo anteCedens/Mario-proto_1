95494eba3eb2fb26269a90dac284aa196b3be0fc
anteCedens <jaksa.juric@yahoo.com.hr>
2021-04-05 21:02:33 +0000

    Add allauth related BACKEND DIRS

--- KIC_comm_garden/settings.py
+++ KIC_comm_garden/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'KIC_comm_garden.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
